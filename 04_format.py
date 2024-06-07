7047599195872fd67b897e0bb8163dac5bcc71b8
Ugonna Ohakim <ugonna.goodness.ohakim@students.iaac.net>
2024-06-07 01:51:49 -0400

    Summary and Format Datasets

--- 04_format.py
+++ 04_format.py
@@ -1,4 +1,3 @@
-from transformers import AutoTokenizer
 import json
 import requests
 import os
@@ -16,26 +15,26 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 # output - what the llm should respond
 
 # Point to the collected text ("the output")
-text_path = "dataset/summary_text.txt"
+text_path = r"C:\Users\ohakimu\OneDrive - Perkins and Will\Desktop\IAAC\Semester 3\Gen AI\LLM\LLM-Finetuning\dataset\summary_text.txt"
 
 # Define the LLM task (the "instruction")
-instruction = "Further develop the concept for the building by developing a detailed description of the project."
+instruction = "Further develop the concept for the restaurant by developing a detailed description of the restaurant."
 
 # Define the LLM task
-task = "You are an API that converts bodies of text into a single instruction object." \
-        "Each JSON should include only an 'instruction' field." \
-        "Create an instruction asking for the design of a building, framing your instruction in such a way that it relates to the idea behind the concept of the design." \
-        "You should only fill in the instruction field with one sentence and it should begin in this format: 'Imagine a building that...' " \
-        "Never directly refer to the name of the buiding or specific details of context of the design in your instruction." \
-        "Only respond in the JSON format and no additional text.\n"
-
-tokenizer = AutoTokenizer.from_pretrained("TheBloke/guanaco-13B-GPTQ")
+task = (
+    "You are an API that converts bodies of text into a single instruction object. "
+    "Each JSON should include only an 'instruction' field. "
+    "Create an instruction asking for the design of a restaurant, framing your instruction in such a way that it relates to the idea behind the concept of the restaurant. "
+    "You should only fill in the instruction field with one sentence and it should begin in this format: 'Imagine a restaurant that...' "
+    "Never directly refer to the name of the restaurant or specific details of the context of the design in your instruction. "
+    "Only respond in the JSON format and no additional text.\n"
+)
 
 # Get instruction pair from LLM (the "input")
 def generate_completion(task, paragraph):
     response = client.chat.completions.create(
-    model="gpt-4-turbo",
-    messages=[
+        model="gpt-4-turbo",
+        messages=[
         {"role": 
             "system", 
             "content": task},
@@ -72,27 +71,27 @@ def get_completion(task, paragraph):
 
 # Run
 # Get our collected text
-with open(text_path, 'r') as file:
+with open(text_path, 'r', encoding='latin-1') as file:
     summaries = file.read()
 
 # Send each paragraph to the LLM to get the paired response
 paragraphs = summaries.split('\n\n')
 responses = []
-for paragraph in paragraphs:
+for idx, paragraph in enumerate(paragraphs):
     response = get_completion(task, paragraph)
-    print(len(responses))
+    print(f"{idx + 1}/{len(paragraphs)}")
     if response is not None:
         # Assemble into the right json format
         paired_response = {
             "instruction": instruction,
             "input": response['instruction'],
             "output": paragraph
-            }
+        }
         responses.append(paired_response)
 
 # Export JSON file
-outpath = 'dataset/structured_dataset.json'
-with open(outpath, 'w') as f:
+outpath = r'C:\Users\ohakimu\OneDrive - Perkins and Will\Desktop\IAAC\Semester 3\Gen AI\LLM\LLM-Finetuning\dataset\structured_dataset.json'
+with open(outpath, 'w', encoding='utf-8') as f:
     json.dump(responses, f, indent=2, ensure_ascii=False)
 
 print(f"Saved {outpath} containing {len(responses)} training examples.")
